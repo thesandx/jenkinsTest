d86dfc13ce93f7bc85cc88c7988d8851042675a7
sandeep <kumarjha.sandeep@gmail.com>
2019-04-12 18:29:27 +0530

    jetkins sample

---
+++ src/test/java/jetkinsTestApp/TestApp.java
@@ -0,0 +1,31 @@
+package jetkinsTestApp;
+
+import java.net.MalformedURLException;
+
+import org.testng.annotations.Test;
+
+import com.sandeep.maven.jenkinsTest.*;
+
+public class TestApp {
+	
+	@Test
+	public void Test() throws MalformedURLException,InterruptedException {
+		
+	/**	WebsiteTest pristyn  = new WebsiteTest();
+		//pristyn.chromeSetup();
+		pristyn.firefoxSetup();
+		pristyn.launchWeb();
+		pristyn.wait(5);
+		//pristyn.fillForm();
+		//pristyn.brokenLinks();
+		//pristyn.navigation();
+		pristyn.navigationByTag();  **/
+		
+		
+		
+		
+		
+	}	
+
+
+}
